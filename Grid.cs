f03fd52ff8de5998f54b93e5366cc1d249b6240a
Matt Staples <itms2@lunet.lboro.ac.uk>
2024-11-27 15:42:51 +0000

    pretty much done

--- Grid.cs
+++ Grid.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Grid : Node2D
 {
@@ -11,7 +12,7 @@ public partial class Grid : Node2D
 	private int tilePaddingWidth = 15;
 	private int tilePaddingHeight = 15;
 	private int startingColumns = 4;
-	private int startingRow = 4;
+	private int startingRow = 2;
 	private int currentColumn;
 	private int currentRow;
 	private PackedScene sceneTile;
@@ -20,17 +21,26 @@ public partial class Grid : Node2D
 	private bool isRight;
 	private Node speedTimer;
 	private bool isMoving = true;
+	private List<int> columnCount = new List<int>();
+	private bool gameOver = false;
+	private Node tileContainer;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
+	{
+		tileContainer = GetNode<Node2D>("TileContainer");
+		PopulateGame();
+	}
+
+	private void PopulateGame()
 	{
 		currentColumn = startingColumns;
 		currentRow = startingRow;
 		grid = new Tile[rows, columns];
 		speedTimer = GetNode<Timer>("SpeedTimer");
 		sceneTile = ResourceLoader.Load<PackedScene>("res://Tile.tscn");
-		currentTile = InstantiateTile(currentColumn, currentRow);
-		GD.Print(currentTile);
+		currentTile = InstantiateTile(currentRow, currentColumn);
+		MoveCurrentTile(currentTile);
 	}
 
 	public override void _Input(InputEvent @event)
@@ -44,7 +54,6 @@ public partial class Grid : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		GD.Print(isMoving);
 	}
 
 	private Vector2 GridReferenceToGridPosition(Vector2 GridReference)
@@ -56,34 +65,37 @@ public partial class Grid : Node2D
 	{
 		Vector2 startPosition = GridReferenceToGridPosition(new Vector2(x, y));
 		Tile newTile = sceneTile.Instantiate() as Tile;
+		newTile.isCurrentTile = true;
+		newTile.UpdateColor(y);
 		grid[x, y] = newTile;
 		newTile.Position = startPosition;
-		AddChild(newTile);
+		tileContainer.AddChild(newTile);
 		return newTile;
 	}
 
 	private void Action()
 	{
+		if (gameOver)
+		{
+			Restart();
+		}
 		if (!gameStart)
 		{
 			gameStart = true;
-			MoveCurrentTile(currentTile);
 		}
 		else
 		{
-			((Timer)speedTimer).WaitTime = ((Timer)speedTimer).WaitTime / 2;
-			isMoving = false;
 			StopBlock();
-			CheckLose();
 		}
 	}
 
 	private async void MoveCurrentTile(Tile moveTile)
 	{
-		while (isMoving)
+		GD.Print("move!");
+		while (isMoving && moveTile.isCurrentTile && !gameOver)
 		{
 			await ToSignal(speedTimer, "timeout");
-			if (isMoving)
+			if (isMoving && moveTile.isCurrentTile)
 			{
 				if (isRight)
 				{
@@ -103,7 +115,6 @@ public partial class Grid : Node2D
 				}
 				grid[currentRow, currentColumn] = moveTile;
 				moveTile.Position = GridReferenceToGridPosition(new Vector2(currentRow, currentColumn));
-				GD.Print("ROW: " + currentRow + "-COLUMN: " + currentColumn);
 			}
 		}
 	}
@@ -112,11 +123,74 @@ public partial class Grid : Node2D
 
 	private void StopBlock()
 	{
+		columnCount.Add(currentRow);
+		CheckWinLose();
+		((Timer)speedTimer).WaitTime = ((Timer)speedTimer).WaitTime / 2;
+		isMoving = false;
+		currentTile.isCurrentTile = false;
+		currentTile = null;
+		currentColumn--;
+		currentRow = 2;
+		currentTile = InstantiateTile(currentRow, currentColumn);
+		isMoving = true;
+		MoveCurrentTile(currentTile);
 
 	}
 
-	private void CheckLose()
+	private void CheckWinLose()
 	{
+		int? firstValue = null;
+		foreach (var item in columnCount)
+		{
+			if (!firstValue.HasValue)
+			{
+				firstValue = item;
+			}
+			if (firstValue == item)
+			{
+				if (columnCount.Count == 5)
+				{
+					GD.Print("You win");
+					gameOver = true;
+					break;
+				}
+				else
+				{
+					GD.Print("Continue!");
+				}
 
+			}
+			else
+			{
+				GD.Print("Lose");
+				gameOver = true;
+				break;
+			}
+		}
+	}
+
+	private void Restart()
+	{
+		foreach (Node item in tileContainer.GetChildren())
+		{
+			item.QueueFree();
+		}
+		for (int x = 0; x < 4; x++)
+		{
+			for (int y = 0; y < 4; y++)
+			{
+				if (grid[x, y] != null)
+				{
+					grid[x, y].QueueFree();
+					grid[x, y] = null;
+				}
+			}
+		}
+		grid = new Tile[rows, columns];
+		columnCount.Clear();
+		gameOver = false;
+		gameStart = false;
+		((Timer)speedTimer).WaitTime = 1;
+		PopulateGame();
 	}
 }
--- Tile.cs
+++ Tile.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class Tile : Polygon2D
 {
+	private Color tileColor;
+	public bool isCurrentTile;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,5 +13,33 @@ public partial class Tile : Polygon2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void UpdateColor(int value)
+	{
+		switch (value)
+		{
+			case (0):
+				tileColor = new Color("eee3da");
+				break;
+			case (1):
+				tileColor = new Color("eddfc8");
+				break;
+			case (2):
+				tileColor = new Color("f2b178");
+				break;
+			case (3):
+				tileColor = new Color("f59562");
+				break;
+			case (4):
+				tileColor = new Color("f57c5f");
+				break;
+		}
+		Color = tileColor;
+	}
+
+	public void Delete()
+	{
+		this.QueueFree();
 	}
 }
